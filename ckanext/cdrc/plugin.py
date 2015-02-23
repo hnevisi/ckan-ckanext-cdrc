798ac03f4c153301fc6b488336e6465dd34553e7
Wen Li <wen.li@ucl.ac.uk>
2015-02-23 18:05:20 +0000

    Customize static text snippets

--- ckanext/cdrc/plugin.py
+++ ckanext/cdrc/plugin.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
@@ -12,3 +13,18 @@ class CdrcPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'cdrc')
         config_['ckan.site_logo'] = '/images/CDRC_logo.png'
+        config_['ckan.site_description'] = dedent(
+            ''' CDRC Data Cloud is a platform for data discovering, data analytics, data comprehension.
+            ''')
+        config_['ckan.favicon'] = '/images/CDRC_fav.png'
+        config_['ckan.site_about'] = dedent(
+            '''
+            <h1>
+            Welcome to the Consumer Data Research Centre (CDRC).
+            </h1>
+            <p>
+            We are an academic led, multi-institution laboratory which discovers, mines, analyses and synthesises consumer-related datasets from around the UK. The CDRC is an ESRC Data Investment.
+            </p>
+            '''
+        )
+        config_['ckan.site_title'] = 'CDRC Data Cloud'
