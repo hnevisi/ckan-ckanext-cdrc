23d19a72416b7bcdca320a700c8b9b51d6271672
Wen Li <spacelis@gmail.com>
2015-08-05 02:00:15 +0100

    Remove graphics

--- graphics/GB/name_map.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-gb = pd.read_csv('GB.csv')
-mapping = gb[['NAME', 'CODE']]
-mapping['uname'] = mapping['NAME'].apply(lambda x: x.lower().replace(' district', '').replace(' london boro', '').replace('city of ', '').replace(' (b)', '').split(' - ')[-1].replace(' ', '-'))
-mapping.to_csv('mapping.csv', index=False)
