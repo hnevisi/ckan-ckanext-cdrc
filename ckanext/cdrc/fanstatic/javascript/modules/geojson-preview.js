dae579edd3f0c6a8a1764102a02d6c71df7f47f3
Wen Li <spacelis@gmail.com>
2015-08-06 04:03:06 +0100

    Add a preview of geojson files on package page

---
+++ ckanext/cdrc/fanstatic/javascript/modules/geojson-preview.js
@@ -0,0 +1,56 @@
+this.ckan.module('geojson-preview', function ($, _) {
+  $(document).ready(function(){
+    $.ajax($('#map-container').attr('data-src')).done(function(data){
+
+      var geojson = JSON.parse(data);
+      var valMin, valMax;
+      for(var i in geojson.features) {
+        var val = geojson.features[i].properties.value
+        if(!valMin || valMin >= val)
+          valMin = val;
+        if(!valMax || valMax <= val)
+          valMax = val;
+      }
+      var colorMap = chroma.scale(['#FFEDA0', '#800026']).domain([valMin, valMax], 7, 'log');
+
+
+      function style(feature) {
+        return {
+          fillColor: colorMap(feature.properties.value).hex(),
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+        };
+      }
+
+      var map = L.map('map-container');
+      var features = L.geoJson(geojson, {style: style})
+      features.addTo(map);
+      map.fitBounds(features.getBounds());
+      var info = L.control();
+
+      var legend = L.control({position: 'bottomright'});
+
+      legend.onAdd = function (map) {
+
+          var div = L.DomUtil.create('div', 'info legend'),
+              grades = colorMap.domain(),
+              labels = [];
+
+          // loop through our density intervals and generate a label with a colored square for each interval
+          for (var i = 0; i < grades.length - 1; i++) {
+              div.innerHTML +=
+                  '<i style="background:' + colorMap(grades[i] + 1) + '"></i> ' +
+                  grades[i].toFixed(2) + (grades[i + 1] ? '&ndash;' + grades[i + 1].toFixed(2) + '<br>' : '+');
+          }
+
+          return div;
+      };
+
+      legend.addTo(map);
+    });
+
+  });
+});
--- ckanext/cdrc/logic/auth.py
+++ ckanext/cdrc/logic/auth.py
@@ -9,10 +9,12 @@ Description: CDRC auth plugin
 
 from ckan.logic import authz
 import ckan.plugins.toolkit as toolkit
+from ckan.logic import get_action
 
+@toolkit.auth_allow_anonymous_access
 def resource_download(context, data_dict):
-    try:
-        context_user = toolkit.c.user
-    except TypeError:
-        context_user = None
-    return {'success': context_user is not None}
+    logged_in = True if context.get('auth_user_obj') else False
+    is_preview = get_action('resource_show')(context, data_dict)['name'] in ['preview.geojson']
+    return {'success': logged_in or is_preview,
+            'msg': 'Please login to download the resources.'
+            }
