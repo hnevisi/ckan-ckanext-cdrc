ef57c944d7cb25a0eba96b139ad895fdefd8f5e7
Wen Li <spacelis@gmail.com>
2016-02-19 16:31:42 +0000

    Change the map of the month to IMD changes

--- ckanext/cdrc/fanstatic/javascript/modules/embedded_datashine.js
+++ ckanext/cdrc/fanstatic/javascript/modules/embedded_datashine.js
@@ -1,7 +1,7 @@
 this.ckan.module('embedded-datashine', function ($, _) {
   $(document).ready(function(){
     // var tileName = ",c11_ew_-QS302EW0002-QS302EW0001-wd-standard_dev-0.470440000-0.052875400-oa-standard_dev-0.465285000-0.088207400-cb-RdYlBu-8-0";
-    var tileName = "http://maps.cdrc.ac.uk/tiles/ttwm_br_v2";
+    var tileName = "http://maps.cdrc.ac.uk/tiles/imd1015change_11l";
     layerData = new ol.layer.Tile({
       title: "",
       source: new ol.source.XYZ({
@@ -9,12 +9,12 @@ this.ckan.module('embedded-datashine', function ($, _) {
         url: tileName + "/{z}/{x}/{y}.png",
         crossOrigin: 'null',
         attributions: [
-          // new ol.Attribution({ 'html': '<br />Census data: National Statistics, licensed under the Open Government Licence v.3.0.<br />' }),
-          // new ol.Attribution({ 'html': 'DataShine was created by Oliver O&apos;Brien and James Cheshire at UCL CASA/UCL Geography.<br />' }),
-          new ol.Attribution({ 'html': 'This map shows the most popular means people used for travelling to work. <br />' }),
+          new ol.Attribution({ 'html': 'This map shows the change of index of multiple deprivation ranks from 2010 to 2015. <br />' }),
+          new ol.Attribution({ 'html': 'Bluer areas are becoming less deprived at a faster rate than redder areas. <br />' }),
+          new ol.Attribution({ 'html': 'A decrease may still indicate a less deprived area in absolute terms. The 2010 rank is based on a fitting to 2011 LSOAs performed by Public Health England. <br />' }),
+          new ol.Attribution({ 'html': 'To search and download the IMD data click <a href="https://data.cdrc.ac.uk/dataset?q=index+of+deprivation+2010+2015" style="text-decoration: underline;">here</a>, or to browse more maps click <a h$
+          new ol.Attribution({ 'html': 'Contains data from gov.uk. Crown copyright and database right 2015. <br />' }),
           new ol.Attribution({ 'html': 'The CDRC are interested in how these patterns vary geographically, and how these outcomes relate to consumption.  <br />' }),
-          new ol.Attribution({ 'html': 'To search and download the travel to work data click <a href="https://data.cdrc.ac.uk/dataset?q=travel+to+work" style="text-decoration: underline;">here</a>, or to browse more maps click <a href="$
-          new ol.Attribution({ 'html': 'Contains ONS, NRS, NISRA data Crown copyright and database right 2015.' }),
           new ol.Attribution({ 'html': '<br /><br /><br />' }),
         ]
       })
