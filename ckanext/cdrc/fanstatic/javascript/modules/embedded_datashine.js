d206aa52d95a93085a51ad2e9f22bb27f31c8e1d
Wen Li <spacelis@gmail.com>
2016-07-20 16:38:21 +0100

    Change the map of month and move all config into js

--- ckanext/cdrc/fanstatic/javascript/modules/embedded_datashine.js
+++ ckanext/cdrc/fanstatic/javascript/modules/embedded_datashine.js
@@ -1,7 +1,9 @@
 this.ckan.module('embedded-datashine', function ($, _) {
   $(document).ready(function(){
     // var tileName = ",c11_ew_-QS302EW0002-QS302EW0001-wd-standard_dev-0.470440000-0.052875400-oa-standard_dev-0.465285000-0.088207400-cb-RdYlBu-8-0";
-    var tileName = "http://maps.cdrc.ac.uk/tiles/countryofbirth";
+    var tileName = "http://maps.cdrc.ac.uk/tiles/popchg_11_14_gblsoa";
+    $('#map-of-the-month-title').html("Small-Area Population Change 2011-2014");
+    $('#map-of-the-month-link').attr("href", "http://maps.cdrc.ac.uk/#/metrics/popchange/default/BTTTFTT/10/-0.1499/51.5200/");
     layerData = new ol.layer.Tile({
       title: "",
       source: new ol.source.XYZ({
@@ -9,8 +11,8 @@ this.ckan.module('embedded-datashine', function ($, _) {
         url: tileName + "/{z}/{x}/{y}.png",
         crossOrigin: 'null',
         attributions: [
-          new ol.Attribution({ 'html': 'This map shows the top country of origin amoung the residents. <br />' }),
-          new ol.Attribution({ 'html': 'To search and download the data in the map click <a href="https://data.cdrc.ac.uk/product/cdrc-2011-census-data-packs" style="text-decoration: underline;">here</a>, or to browse more maps click <a href="http://maps.cdrc.ac.uk" style="text-decoration: underline;">here</a>. <br />' }),
+          new ol.Attribution({ 'html': 'This map shows the population change in small-area mid-year population estimates by the ONS and NRS, between 2011 and 2014, for the approximately 40,000 LSOAs/DZs across Great Britain (England, Sc$
+          new ol.Attribution({ 'html': 'To search and download the data in the map click <a href="https://data.cdrc.ac.uk/dataset/small-area-population-change-2011-14" style="text-decoration: underline;">here</a>, or to browse more maps$
           new ol.Attribution({ 'html': 'Contains data from gov.uk. Crown copyright and database right 2015. <br />' }),
           new ol.Attribution({ 'html': 'The CDRC are interested in how these patterns vary geographically, and how these outcomes relate to consumption.  <br />' }),
           new ol.Attribution({ 'html': '<br /><br /><br />' }),
@@ -54,4 +56,5 @@ this.ckan.module('embedded-datashine', function ($, _) {
 
   });
 });
+
 
