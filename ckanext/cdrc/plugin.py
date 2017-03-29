37f125caa12fb44326c1448656b1dbfd92d3eb3f
Wen Li <spacelis@gmail.com>
2017-03-29 16:13:08 +0100

    Fix #40: dataset menu appear non-active when it should be
    The static link for dataset menu will not change to gray when active.
    Now when the menu item is clicked, it will stay gray until switching to a different page.

--- ckanext/cdrc/plugin.py
+++ ckanext/cdrc/plugin.py
@@ -172,6 +172,7 @@ class CdrcPlugin(plugins.SingletonPlugin):
         map.connect('/testing/assertfalse', controller='ckanext.cdrc.plugin:CDRCExtController', action='assertfalse')
         map.connect('blog', '/blog', controller='ckanext.cdrc.controllers.page:CDRCBlogController', action='blog_proxy')
         map.connect('practical', '/practical', controller='ckanext.cdrc.controllers.page:CDRCPageController', action='index')
+        map.connect('dataset', '/dataset?q=&sort=accesslevel+desc', controller='package', action='search')
         map.connect('national', '/national', controller='ckanext.cdrc.controllers.singlegroup:SingleGroupController', action='read_national')
         map.connect('regional', '/regional', controller='ckanext.cdrc.controllers.singlegroup:SingleGroupController', action='read_regional')
         with SubMapper(map, controller='ckanext.cdrc.controllers.page:CDRCPageController') as m:
